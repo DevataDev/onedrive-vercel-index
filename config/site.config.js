5574ef7870f116162f2f32a17a24a2a7ffc770e4
DevataDev <devatadev@pm.me>
2022-10-19 12:02:04 +0700

    add more protected path

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🔐 Private Room','/🚀 Gems','/📖 Library'],
+  protectedRoutes: ['/🔐 Private Room','/🚀 Gems','/📖 Library','/📀 Master'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:devatadev@pm.me',
